101523bc1cbb7b1d8b398bc17f6bbd2377a4fd1b
ally <3210jr@gmail.com>
2022-03-31 18:08:52 +0300

    update distribution creation tests

---
+++ test/lib/distributions.test.ts
@@ -0,0 +1,52 @@
+import fc from 'fast-check';
+import {
+	createCauchy,
+	createNormal,
+	createWeibull,
+} from '../../src/lib/distributions';
+
+describe('Creation of Random Variables works', () => {
+	it('Creates a normal variable', () => {
+		fc.assert(
+			fc.property(
+				fc.float(),
+				fc.float({ next: false, min: 0.01 }),
+				(a, b) => {
+					const rv = createNormal('testNormal', a, b);
+					rv._.dist === 'normal';
+					rv.mean = a;
+					rv.sd = b;
+				}
+			)
+		);
+	});
+
+	it('Creates a weibull variable', () => {
+		fc.assert(
+			fc.property(
+				fc.float({ next: false, min: 0.01 }),
+				fc.float({ next: false, min: 0.01 }),
+				fc.float({ next: false, min: 0.01 }),
+				(a, b, c) => {
+					const rv = createWeibull('testWeibull', a, b, c);
+					rv._.dist === 'weibull';
+					rv.threshold === a;
+					rv.shape === b;
+					rv.scale === c;
+				}
+			)
+		);
+	});
+
+	it('Creates a cauchy variable', () => {
+		fc.assert(
+			fc.property(fc.float(), fc.float(), (a, b) => {
+				const rv = createCauchy('testCauchy', a, b);
+
+				rv._.dist === 'cauchy';
+				rv.scale === b;
+				rv.location === a;
+			})
+		);
+	});
+});
